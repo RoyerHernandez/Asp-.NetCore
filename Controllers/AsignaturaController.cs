cd99a7260cdaa7cd53120c33e3efb64717dfa494
royer <ing.royerhernandez@gmail.com>
2020-02-11 20:47:48 -0500

    Vistas Parciales

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -1,19 +1,42 @@
 
 
 using System;
+using System.Collections.Generic;
 using Asp_.NetCore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 public class AsignaturaController: Controller{
-    public IActionResult index(){  
+    
+    public IActionResult Index(){  
+    
+    return View(new Asignatura{Nombre="Matemáticas", UniqueId= Guid.NewGuid().ToString()} );
+    }
+    
+    
+    public IActionResult MultiAsignatura(){  
 
-        var asignatura = new Asignatura{
-            UniqueId = Guid.NewGuid().ToString(),
-            Nombre = "Programación"
-        };
+       var listaAsignaturas = new List<Asignatura>(){
+           
+                            new Asignatura{Nombre="Matemáticas",
+                                UniqueId= Guid.NewGuid().ToString()
+                            } ,
+                            new Asignatura{Nombre="Educación Física",
+                                UniqueId= Guid.NewGuid().ToString()
+                            },
+                            new Asignatura{Nombre="Castellano",
+                                UniqueId= Guid.NewGuid().ToString()
+                            },
+                            new Asignatura{Nombre="Ciencias Naturales",
+                                UniqueId= Guid.NewGuid().ToString()
+                            }
+                            ,
+                            new Asignatura{Nombre="Programación",
+                                UniqueId= Guid.NewGuid().ToString()
+                            }
+                };
         ViewBag.cosaDinamica = "Bienvenidos";
         ViewBag.fecha = DateTime.Now;
-        return View(asignatura);
+        return View("MultiAsignatura",listaAsignaturas);
     }
 
 }
