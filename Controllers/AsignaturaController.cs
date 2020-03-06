4ae23debf42cd3acc20dcbdf9e378b155e30c90f
royer <ing.royerhernandez@gmail.com>
2020-03-05 21:41:52 -0500

    Ajuste de enrutamiento

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -8,8 +8,21 @@ using Microsoft.AspNetCore.Mvc;
 
 public class AsignaturaController: Controller{
     
-    public IActionResult Index(){  
+   /* public IActionResult Index(){  
             return View(_context.Asignaturas.FirstOrDefault());
+    }*/
+    [Route("Asignatura/Index/{asignaturaId}")]
+    public IActionResult Index(string asignaturaId){ 
+
+            if(!string.IsNullOrWhiteSpace(asignaturaId)){
+            var asignatura = from asig in _context.Asignaturas
+                                where  asig.Id == asignaturaId
+                                select asig;
+            return View(asignatura.SingleOrDefault());
+            }
+            else{
+            return View("MultiAsignatura",_context.Asignaturas);        
+            }
     }
     
     
